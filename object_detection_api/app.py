5176eca87ba7378f8644621e22e855b279521f6d
hrxorxm <twinha0204@gmail.com>
2021-02-28 22:53:28 +0900

    flask : 음식 인식 + 영양소 값 가져오기

--- object_detection_api/app.py
+++ object_detection_api/app.py
@@ -1,17 +1,32 @@
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from PIL import ImageDraw
+import pandas as pd
+import os
+
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-app = Flask(__name__)
+import urllib.request
+import urllib
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
 
-# 업로드 HTML 렌더링
-@app.route('/')
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/getimg')
 def main():
     return render_template('upload.html')
 
 # 저장한 이미지를 object detection 코드로 음식 이름값 str로 리턴하기
 
-#리턴한 str값을 바탕으로 api키를 이용해 식품안전db에서 값들 다 가져와서 뿌리기
 
-
+@app.route('/getfoodname')
+def inputfoodname():
+    return render_template('inputfoodname.html')
 
 # 파일 업로드 처리 - 이미지 파일에 저장하기
 @app.route('/imgModel', methods = ['GET', 'POST'])
@@ -21,7 +36,80 @@ def imgModel():
         # 저장할 경로 + 파일명
         f.save(secure_filename(f.filename))
         msg = "uploads success"
-        return render_template('after.html',data = msg)
+
+        # 예측
+        # Load the TFLite model and allocate tensors.
+        interpreter = tf.lite.Interpreter(model_path="model/model.tflite")
+        interpreter.allocate_tensors()
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        # Make Input data
+        input_shape = input_details[0]['shape']
+        image_size = (input_shape[1], input_shape[2])
+        img = Image.open(f.filename)
+        img = img.resize(image_size)
+        num_img = np.array(img)
+        num_img = num_img.reshape(input_shape)
+        interpreter.set_tensor(input_details[0]['index'], num_img)
+        interpreter.invoke()
+        # Make Output data
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        num_det = int(interpreter.get_tensor(output_details[3]['index']))
+        boxes = interpreter.get_tensor(output_details[0]['index'])[0][:num_det]
+        classes = interpreter.get_tensor(output_details[1]['index'])[0][:num_det]
+        scores = interpreter.get_tensor(output_details[2]['index'])[0][:num_det]
+        # Draw results
+        df = pd.DataFrame(boxes)
+        img_width, img_height = img.size
+        df['ymin'] = df[0].apply(lambda y: max(1,(y*img_height)))
+        df['xmin'] = df[1].apply(lambda x: max(1,(x*img_width)))
+        df['ymax'] = df[2].apply(lambda y: min(img_height,(y*img_height)))
+        df['xmax'] = df[3].apply(lambda x: min(img_width,(x * img_width)))
+        boxes_scaled = df[['ymin', 'xmin', 'ymax', 'xmax']].to_numpy()
+        draw = ImageDraw.Draw(img)
+        for i in range(num_det):
+            if scores[i] > 0.5:
+                x1, y1, x2, y2 = boxes_scaled[i]
+                draw.rectangle(((x1, y1), (x2, y2)), outline=(0, 0, 255), width=1)
+                msg = classes[i]
+        pred_filename = "pred_" + f.filename
+        img.save("static/"+pred_filename)
+
+        return render_template('after.html', data=msg, filename=pred_filename)
+
+#이미지 이름 가져와서 영양소 값들 출력하기
+@app.route('/getfoodname', methods=['POST'])
+def getfoodname():
+    #foodname 받아오기
+    foodname=request.form['a']
+    with open('secret.json') as json_file:
+        json_data = json.load(json_file)
+    accesskey=json_data['accesskey']
+    encoding_foodname=urllib.parse.quote(foodname)
+
+    url = "http://openapi.foodsafetykorea.go.kr/api/"+accesskey+"/I2790/json/1/21/DESC_KOR="+encoding_foodname
+
+    response = urllib.request.urlopen(url)
+    json_str = response.read().decode("utf-8")
+    json_object = json.loads(json_str)
+
+    df=pd.json_normalize(json_object['I2790']['row'])
+    send_data_list=list()
+
+    send_data_list.append(df['DESC_KOR'][0])
+
+    for i in range(1,10):
+    #순서대로 음식명 열량 / 탄수화물 / 단백질 / 지방 / 당류 / 나트륨 / 콜레스테롤 / 포화지방산 / 트랜스지방
+        idxname='NUTR_CONT'
+        idxname=idxname+str(i)
+        value=df[idxname][0]
+        send_data_list.append(value)
+
+    send_data_list_num = len(send_data_list)
+
+    return render_template('after2.html', data=send_data_list, num=send_data_list_num)
+
 
 if __name__ == '__main__':
     # 서버 실행
---
+++ object_detection_api/server.py
@@ -0,0 +1,84 @@
+import urllib.request
+import urllib
+import json
+import pandas as pd
+import logging
+from pandas.io.json import json_normalize
+
+def getfoodname():
+    """이미지 인식해서 모델명 받아오는 코드"""
+    """여기서 함수를 3개 만들어서 함수명으로 알아보기 쉽게 하는게 좋을지 아니면 getfoodname에서 nurtrients불러와서 한번에 리턴하는게 좋을지.."""
+
+
+def getnutrients(foodname):
+
+    with open('secret.json') as json_file:
+        json_data = json.load(json_file)
+    accesskey=json_data['accesskey']
+    encoding_foodname=urllib.parse.quote(foodname)
+
+    url = "http://openapi.foodsafetykorea.go.kr/api/"+accesskey+"/I2790/json/1/21/DESC_KOR="+encoding_foodname
+
+    response = urllib.request.urlopen(url)
+    json_str = response.read().decode("utf-8")
+    json_object = json.loads(json_str)
+
+    df=pd.json_normalize(json_object['I2790']['row'])
+    send_data_list=list()
+
+    send_data_list.append(df['DESC_KOR'][0])
+
+    for i in range(1,10):
+    #순서대로 음식명 열량 / 탄수화물 / 단백질 / 지방 / 당류 / 나트륨 / 콜레스테롤 / 포화지방산 / 트랜스지방
+        idxname='NUTR_CONT'
+        idxname=idxname+str(i)
+        value=df[idxname][0]
+        send_data_list.append(value)
+
+    return send_data_list
+
+
+def __get_logger():
+    __logger = logging.getLogger('logger')
+
+    # 로그 포멧 정의
+    formatter = logging.Formatter(
+        'BATCH##AWSBATCH##%(levelname)s##%(asctime)s##%(message)s >> @@file::%(filename)s@@line::%(lineno)s')
+    # 스트림 핸들러 정의
+    stream_handler = logging.StreamHandler()
+    # 각 핸들러에 포멧 지정
+    stream_handler.setFormatter(formatter)
+    # 로거 인스턴스에 핸들러 삽입
+    __logger.addHandler(stream_handler)
+    # 로그 레벨 정의
+    __logger.setLevel(logging.DEBUG)
+
+    return __logger
+
+
+if __name__ == "__main__":
+    print("start")
+    logger = __get_logger()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    # 소켓 레벨과 데이터 형태를 설정한다.
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
+    # 서버는 복수 ip를 사용하는 pc의 경우는 ip를 지정하고 그렇지 않으면 None이 아닌 ''로 설정한다.
+    # 포트는 pc내에서 비어있는 포트를 사용한다. cmd에서 netstat -an | find "LISTEN"으로 확인할 수 있다.
+    server_socket.bind(('', 9000));
+    # server 설정이 완료되면 listen를 시작한다.
+    server_socket.listen();
+
+    try:
+        # 서버는 여러 클라이언트를 상대하기 때문에 무한 루프를 사용한다.
+        while True:
+            # client로 접속이 발생하면 accept가 발생한다.
+            # 그럼 client 소켓과 addr(주소)를 튜플로 받는다.
+            client_socket, addr = server_socket.accept();
+            th = threading.Thread(target=binder, args = (client_socket,addr));
+            # 쓰레드를 이용해서 client 접속 대기를 만들고 다시 accept로 넘어가서 다른 client를 대기한다.
+            th.start();
+    except:
+        print("server");
+    finally:
+            # 에러가 발생하면 서버 소켓을 닫는다.
+        server_socket.close();
